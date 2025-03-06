fdffd3190ed41a4d68165b741335a79cb2fd50f9
Arta <arta.mirzadeh@gmail.com>
2025-03-05 17:53:52 -0800

    image col in posts table

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,20 +8,23 @@
             
                 <h1 class="display-5 fw-bold">Create A New Post</h1>
                 <hr>
-                    <form action="{{route('posts.store') }}" method="POST" data-parsley-validate>
+                    <form action="{{route('posts.store') }}" method="POST" enctype="multipart/form-data" data-parsley-validate>
                         @csrf
-                         <label for="title">Title:</label>
-                         <input type="text" name="title" id="title" class="form-control" required>
+                            <label for="title">Title:</label>
+                            <input type="text" name="title" id="title" class="form-control" required>
 
-                         <label for="slug">URL Slug:</label>
-                         <input type="text" name="slug" id="slug" class="form-control" maxlength="255" minlength="5" required>
+                            <label for="slug">URL Slug:</label>
+                            <input type="text" name="slug" id="slug" class="form-control" maxlength="255" minlength="5" required>
 
-                        <label for="body">Post Content</label>
-                        <textarea id="ckeditor" name="body" class="form-control" rows="5"></textarea><br>
+                            <label for="post_img">Photo:</label>
+                            <input type="file" name="post_img" class="form-control">
 
-                        <div class="d-grid gap-2 mt-1">
-                        <button type="submit" class="btn btn-dark"> Create Post</button>
-                        </div> 
+                            <label for="body">Post Content</label>
+                            <textarea id="ckeditor" name="body" class="form-control" rows="5"></textarea><br>
+
+                            <div class="d-grid gap-2 mt-1">
+                            <button type="submit" class="btn btn-dark"> Create Post</button>
+                            </div> 
 
                     </form>
                   
