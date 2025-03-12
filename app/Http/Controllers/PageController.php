783954628af1b154ea60ce3bc28bb2e9f01d5d26
Arta <arta.mirzadeh@gmail.com>
2025-03-12 12:07:43 -0700

    smpt mail config

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -27,7 +27,7 @@ class PageController extends Controller
         
     }
     public function postContact(Request $request) {
-        $this->validate($request, [
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'subject' => 'required|min:3|max:255',
             'message' => 'required|min:10|max:1000'
