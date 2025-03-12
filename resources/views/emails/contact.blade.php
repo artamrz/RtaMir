7820b227e372528fef6853f968ba8d96f456d76e
Arta <arta.mirzadeh@gmail.com>
2025-03-12 12:17:20 -0700

    smpt mail config

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,4 +1,4 @@
 <h3>You have a New Message via the Contact Form</h3>
 <p><strong>Name:</strong> {{ $email }}</p>
 <p><strong>Email:</strong> {{ $subject }}</p>
-<p><strong>Message:</strong> {{ $message }}</p>
+<p><strong>Message:</strong> {{ $bodyMessage }}</p>
