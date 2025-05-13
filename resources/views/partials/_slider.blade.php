4c4363af732d36b1a6e3c5ee4f7a08a9005db65b
Arta <arta.mirzadeh@gmail.com>
2025-05-13 08:11:23 -0700

    home heroe

--- resources/views/partials/_slider.blade.php
+++ resources/views/partials/_slider.blade.php
@@ -1,10 +1,14 @@
 
 <!-- Video Section -->
  <div class="container-fluid homepage-video py-5">
-    <video src="video/CodingUniverse-HomePageVideo.mp4" autoplay muted loop></video>
+    <video autoplay muted loop>
+        <source src="video/CodingUniverse-HomePageVideo.mp4" type="video/mp4">
+        <source src="video/CodingUniverse-HomePageVideo.webm" type="video/webm">
+        Your browser does not support the video tag.  
+    </video>
     <div class="video-content">
         <h1 class="display-5 fw-bold mt-5">This is Your Gateway</h1>
-        <p class="fs-4 pb-5">of joiningthe world of coding and technology, Let's dive deep...</p>
+        <p class="fs-4 pb-5">of joining the world of coding and technology, Let's dive deep...</p>
     </div>
     
 </div> 
