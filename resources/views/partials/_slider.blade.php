f421d641f64c4107787c1a413eddb175fb0ffadb
Arta <arta.mirzadeh@gmail.com>
2025-05-13 13:38:35 -0700

    post card

--- resources/views/partials/_slider.blade.php
+++ resources/views/partials/_slider.blade.php
@@ -1,15 +1,16 @@
 
 <!-- Video Section -->
- <section class="homepage-video py-5">
-    <video autoplay muted loop playsinline>
-        <source src="video/CodingUniverse-HomePageVideo.mp4" type="video/mp4">
-        <source src="video/CodingUniverse-HomePageVideo.webm" type="video/webm">
-        Your browser does not support the video tag.  
-    </video>
-    <div class="video-content mt-5 mx-4">
-        <h1 class="display-5 fw-bold mt-5">This is Your Gateway</h1>
-        <p class="fs-4 pb-5">of joining the world of coding and technology, Let's dive deep...</p>
-    </div>
+<section class="homepage-video">
+  <video autoplay muted loop playsinline class="bg-video">
+    <source src="video/CodingUniverse-HomePageVideo.mp4" type="video/mp4">
+    <source src="video/CodingUniverse-HomePageVideo.webm" type="video/webm">
+    Your browser does not support the video tag.
+  </video>
+
+  <div class="video-content text-center text-white">
+    <h1 class="display-5 fw-bold mt-5">This is Your Gateway</h1>
+    <p class="fs-4 pb-5">of joining the world of coding and technology. Let's dive deep...</p>
+  </div>
 </section>
 <!-- End of Video Section -->
 
